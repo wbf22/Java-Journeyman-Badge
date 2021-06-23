cdac6443c7281aed1a5eaaeb9bc862616088d346
wbf22 <brandon@fivefowlers.com>
2021-06-23 12:36:19 -0600

    grapher is pretty much done uncless we want it to do functions

--- src/main/java/com/nonagon/javajourneymanbadge/graphing/Grapher.java
+++ src/main/java/com/nonagon/javajourneymanbadge/graphing/Grapher.java
@@ -1,5 +1,7 @@
 package com.nonagon.javajourneymanbadge.graphing;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.*;
 
 public class Grapher {
@@ -21,16 +23,22 @@ public class Grapher {
 //           5       10      15
 
 
-  private int step = 5; //spacing of grid
-  private int width = 20; //number of grid squares x
-  private int height = 20; //number of grid squares y
+  private BigDecimal spacing = BigDecimal.valueOf(.2);
+  private int width = 80; //number of grid squares x
+  private int height = 80; //number of grid squares y
   private String[][] currentGraphState = new String[height + 3][width + 4];
   //auxiliary params
+  private BigDecimal step = spacing.multiply(BigDecimal.valueOf(2)); //spacing of grid
   private String leftPadding = "         ";
   private String emptyCell = "    ";
+  private MathContext mContext = new MathContext(step.precision());
 
-  public void graph(Map<Float, Float> points, ArrayList<String> functions){
+  public void graph(Map<BigDecimal, BigDecimal> points, ArrayList<String> functions){
     initGraph();
+
+    for (Map.Entry<BigDecimal, BigDecimal> entry : points.entrySet()) {
+      plotPoint(entry.getKey(), entry.getValue());
+    }
 
     printGraph();
 
@@ -55,40 +63,55 @@ public class Grapher {
   private void initGraph(){
 
 
-    for (int i = 2; i < height + 1; i++){
+    for (int i = 2; i < height + 2; i++){
       currentGraphState[i] = toArrayFromList(makeRow(whiteSpace(4)));
-      currentGraphState[i][1] = "|";
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append(currentGraphState[i][1]);
+      stringBuilder.setCharAt(0, '|');
+      currentGraphState[i][1] = stringBuilder.toString();
     }
 
     currentGraphState[1] = toArrayFromList(makeRow("___ "));
-    currentGraphState[1][1] = "|";
+    StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append(currentGraphState[1][1]);
+    stringBuilder.setCharAt(0, '|');
+    currentGraphState[1][1] = stringBuilder.toString();
 
     currentGraphState[0] = toArrayFromList(makeRow(whiteSpace(4)));
     addNumberLabel();
 
   }
-  private void plotPoint(float x, float y){
-    float cellSize = ((float) step) / 2.0f;
-
+  private void plotPoint(BigDecimal x, BigDecimal y){
+    int xc = getCellForPoint(x);
+    int yc = getCellForPoint(y);
+    if (xc + 1 < width && yc + 2 < height){
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append(currentGraphState[yc+2][xc+1]);
+      stringBuilder.setCharAt(1, '*');
+      currentGraphState[yc+1][xc+1] = stringBuilder.toString();
+    }
   }
 
   //helper functions
+  private int getCellForPoint(BigDecimal p){
+    return (p.divide(spacing)).round(new MathContext(0)).intValue();
+  }
   private void addNumberLabel(){
     //vertical axis
-    int vLabel = step;
-    for (int i = 2; i < height + 1; i+=2){
-      currentGraphState[i][0] = injectNummberInPadding(vLabel);
-      vLabel += step;
+    BigDecimal vLabel = BigDecimal.valueOf(0.0);
+    for (int i = 1; i < height + 2; i+=2){
+      currentGraphState[i][0] = injectNumberInPadding(vLabel);
+       vLabel = vLabel.add(step);
     }
 
     //horizontal axis
-    int hLabel = step;
+    BigDecimal hLabel = step;
     currentGraphState[0][0] = leftPadding;
-    currentGraphState[0][1] = " ";
+    currentGraphState[0][1] = injectNumberInCell(BigDecimal.valueOf(0.0));
     for (int i = 3; i < width + 2; i+=2){
       currentGraphState[0][i - 1] = whiteSpace(4);
       currentGraphState[0][i] = injectNumberInCell(hLabel);
-      hLabel += step;
+      hLabel = hLabel.add(step);
     }
 
 
@@ -108,8 +131,8 @@ public class Grapher {
     }
     return stringBuilder.toString();
   }
-  private String injectNummberInPadding(int num){
-    String numToString = Integer.toString(num);
+  private String injectNumberInPadding(BigDecimal num){
+    String numToString = num.toString();
     int padding = leftPadding.length() - numToString.length();
     StringBuilder stringBuilder = new StringBuilder();
     stringBuilder.append(whiteSpace(padding));
@@ -117,14 +140,17 @@ public class Grapher {
     return stringBuilder.toString();
 
   }
-  private String injectNumberInCell(int num){
-    String numToString = Integer.toString(num);
-    if (numToString.length() > emptyCell.length() - 1){
-      return " " + numToString.charAt(0) + "." + numToString.charAt(1) + "k";
+  private String injectNumberInCell(BigDecimal num){
+    String numToString = num.toString();
+    if (numToString.length() > emptyCell.length()){
+      StringBuilder stringBuilder = new StringBuilder();
+      stringBuilder.append(numToString.charAt(0));
+      stringBuilder.append(numToString.charAt(1));
+      stringBuilder.append(numToString.charAt(2));
+      return stringBuilder.toString() + "-";
     }
-    int padding = emptyCell.length() - numToString.length() - 1;
+    int padding = emptyCell.length() - numToString.length();
     StringBuilder stringBuilder = new StringBuilder();
-    stringBuilder.append(" ");
     stringBuilder.append(numToString);
     stringBuilder.append(whiteSpace(padding));
     return stringBuilder.toString();
@@ -138,7 +164,15 @@ public class Grapher {
 
   public static void main(String args[]){
     Grapher grapher = new Grapher();
-    grapher.graph(null, null);
+    HashMap<BigDecimal, BigDecimal> points = new HashMap<>();
+    points.put(new BigDecimal("0"), new BigDecimal("0"));
+    points.put(new BigDecimal("0.6"), new BigDecimal("1.2"));
+    points.put(new BigDecimal("0.3"), new BigDecimal("0.1"));
+    points.put(new BigDecimal("0.3"), new BigDecimal("1.1"));
+    points.put(new BigDecimal("0.7"), new BigDecimal("0.4"));
+
+
+    grapher.graph(points, null);
   }
 
 
